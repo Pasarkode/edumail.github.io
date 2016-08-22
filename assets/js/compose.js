98d928a6a94c3ac0d1794b7d7dcb707820a173c7
Arif Lutfhansah <Arif@Arifs-iMac.local>
2016-08-22 12:59:53 +0700

    Inbox Page

--- assets/js/compose.js
+++ assets/js/compose.js
@@ -6,14 +6,16 @@ $(".enter-mail-id").keydown(function (e) {
 	 $(this).val('');
   }
 });
-
-
+$(".enter-cc-id").keydown(function (e) {
+  if (e.keyCode == 13 || e.keyCode == 32) {
+    //alert('You Press enter');
+	 var getValue = $(this).val();
+	 $('.all-cc').append('<span class="email-ids">'+ getValue +' <span class="cancel-email">x</span></span>');
+	 $(this).val('');
+  }
+});
 /// Cancel 
-
-$(document).on('click','.cancel-email',function(){
-	  
+$(document).on('click','.cancel-email',function(){  
 	      $(this).parent().remove();
-	
 	});
-
   // $('.enter-mail-id').click()
