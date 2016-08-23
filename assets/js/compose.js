acc9b2c8a2330b27123999031b7f72d2bcb35189
Arif Lutfhansah <Arif@Arifs-iMac.local>
2016-08-23 12:36:25 +0700

    Compose Page

--- assets/js/compose.js
+++ assets/js/compose.js
@@ -14,6 +14,14 @@ $(".enter-cc-id").keydown(function (e) {
 	 $(this).val('');
   }
 });
+$(".enter-bcc-id").keydown(function (e) {
+  if (e.keyCode == 13 || e.keyCode == 32) {
+    //alert('You Press enter');
+	 var getValue = $(this).val();
+	 $('.all-bcc').append('<span class="email-ids">'+ getValue +' <span class="cancel-email">x</span></span>');
+	 $(this).val('');
+  }
+});
 /// Cancel 
 $(document).on('click','.cancel-email',function(){  
 	      $(this).parent().remove();
